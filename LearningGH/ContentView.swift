3f9bbdb9b2785c462bfa887a0181083efea78eb0
Tyler Skwara <tskwara12@gmail.com>
2023-12-29 14:29:29 -0600

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Tyler Skwara on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
